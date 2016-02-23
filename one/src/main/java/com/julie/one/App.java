c889532783fbe6609ea22543953554f15da97c0c
Julie Mather <julie.mather@plastiq.com>
2016-02-23 09:02:36 -0800

    New dummy project to test release plugin

---
+++ one/src/main/java/com/julie/one/App.java
@@ -0,0 +1,13 @@
+package com.julie.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
